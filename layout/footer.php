01b2395ee37cb565b30c97077086daa18aff89fd
BartHxC <barthxc.business@gmail.com>
2023-12-06 00:21:50 +0100

    agrego header y footer a mi layout con estilos para las futura tabla que crearé para las peliculas. tambien creo datos estáticos que cambiaré con la sesión de php

---
+++ layout/footer.php
@@ -0,0 +1,5 @@
+<footer class="mt-auto flex flex-col text-gray-400 gap-2 justify-center text-center mb-3 sm:flex-row sm:gap-20">
+    <p>Pablo Bartolomé Gallardo</p>
+    <a class="hover:text-gray-300" href="">¿Quién usa nuestro servicio?</a>
+</footer>
+</body>
---
+++ layout/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>WatchVault</title>
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="styles.css" />
+</head>
+
+<body>
+
+    <div class="flex flex-col mb-10 justify-evenly text-gray-400 items-center sm:flex-row">
+        <div class="flex flex-row gap-10 py-10 justify-center font-bold text-4xl">
+            <a href="" class="hover:text-gray-300">Inicio</a>
+            <a href="" class="hover:text-gray-300">Vault</a>
+            <a href="" class="hover:text-gray-300">Doc</a>
+        </div>
+
+        <div class="flex flex-col gap-2 text-base">
+            <p class="font-bold">Invitado</p>
+            <a href="" class="hover:text-gray-300">Login</a>
+        </div>
+    </div>
